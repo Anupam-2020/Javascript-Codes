9a92c7c5413ccee11dd4fe3072c65d35a83ab497
Anupam-2020 <anupamanand.696@gmail.com>
2023-08-18 01:49:30 +0530

    added fibonacci series

---
+++ javascript_dsa/part_1/fibonacci_series.js
@@ -0,0 +1,26 @@
+// fibonacci series....
+function fibonacci(nth_value) {
+    let firstValue = 0;
+    let secondValue = 1;
+    // console.log(`${firstValue} \n${secondValue}`)
+    for(let i = 2; i < nth_value; i++) {
+        let temp = firstValue;
+        firstValue = secondValue;
+        secondValue = firstValue + temp;
+        // console.log(secondValue);
+    }
+    console.log(secondValue)
+}
+
+function fibRecursion(nth_value) {
+    if(nth_value <= 1) {
+        return nth_value
+    }
+    
+    return fibRecursion(nth_value - 1) + fibRecursion(nth_value - 2);
+}
+
+console.time("fib")
+fibonacci(6)
+// console.log(fibRecursion(6))
+console.timeEnd("fib")
