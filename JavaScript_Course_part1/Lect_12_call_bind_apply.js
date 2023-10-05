4182f091462462c567ac7511330df04c4f6c716d
Anupam-2020 <anupamanand.696@gmail.com>
2023-10-05 14:56:07 +0530

    added some polyfills on bind apply  and call methods

--- JavaScript_Course_part1/Lect_12_call_bind_apply.js
+++ JavaScript_Course_part1/Lect_12_call_bind_apply.js
@@ -22,3 +22,61 @@ about.apply(user1, ['Coding', 'Ludo']);
 // bind
 const func = about.bind(user2);
 func('Coding', 'Basket Ball');
+
+
+// creating polyfills for call, bind and apply methods...
+let car = {
+    name: 'Tata',
+    model: 'Nexon'
+}
+  
+function purchaseCar(price, color) {
+    console.log(`Purchased ${this.name} ${this.model} in ${color} color at Rs.${price}`);
+}
+
+
+// polyfill for call...
+Function.prototype.myCall = function(context, ...args) {
+    if(typeof this !== 'function') {
+      throw new Error(this + " It's not callable");
+    }
+  
+    context.fn = this; // this points to the purchaseCar function and fn key gets created here...
+    context.fn(...args); // we need to paas arguments to the obj to make the function-key access those values...
+    // Note:- The above steps will create a function same as purchaseCar in the object(car)...
+}
+
+purchaseCar.myCall(car, '10,00,000', 'black');
+
+
+
+// polyfill for apply.
+Function.prototype.myApply = function(context, args) {
+    if(typeof this !== 'function') {
+      throw new Error(this+ ' It"s not a function');
+    }
+  
+    if(!Array.isArray(args)) {
+      throw new TypeError("Invalid arguments");
+    }
+    context.fn = this;
+    context.fn(...args);
+}
+  
+purchaseCar.myApply(car, ['10,00,000', 'black']);
+
+
+// polyfill for bind
+Function.prototype.myBind = function(context, ...args) {
+    if(typeof this !== 'function') {
+      throw new Error(this+ ' This is not a function');
+    }
+  
+    context.fn = this;
+    return function(...newArgs) {
+      context.fn(...newArgs, ...args);
+    }
+  }
+  
+  const fun = purchaseCar.myBind(car, 'black');
+  fun('10,00,000')
--- practice/index.js
+++ practice/index.js
@@ -25,36 +25,24 @@
 // arr.splice(0,arr.length);
 // console.log(arr); 
 
-function myMemoization(fn, context) {
-    res = {};
-    return function(...args) {
-        // console.log(JSON.stringify(args));
-        let argsCache = JSON.stringify(args);
-        if(!res[argsCache]) {
-            // res[argsCache] = fn.call(context || this, ...args);
-  
-            // const func = fn.bind(context || this);
-            // res[argsCache] = func(...args);
-  
-            // res[argsCache] = fn.apply(context || this, arguments);
-  
-            res[argsCache] = fn(...args)
-        } 
-        return res[argsCache];
-    }
+let car = {
+  name: 'Tata',
+  model: 'Nexon',
+}
+
+function purchaseCar(price, color) {
+  console.log(`Purchased ${this.name} ${this.model} in ${color} color at Rs.${price}`);
+}
+
+// polyfill for bind...
+Function.prototype.myCall = function(context, ...args) {
+  if(typeof this !== 'function') {
+    throw new Error(this + " It's not callable");
   }
-  
-  function product(num1, num2) {
-    for(let i = 0; i < 100000000; i++) {}
-    return num1 * num2;
-  }
-  
-  const memoizedProduct = myMemoization(product);
-  
-  console.time("first call");
-  console.log(memoizedProduct(23,45));
-  console.timeEnd("first call");
-  
-  console.time("2nd call");
-  console.log(memoizedProduct(23,45))
-  console.timeEnd("2nd call");
+
+  context.fn = this;
+  context.fn(...args);
+}
+
+purchaseCar.myCall(car, '10,00,000', 'black');
+car.fn('10,00,000', 'black')
